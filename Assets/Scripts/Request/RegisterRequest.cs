1e135fcd450ad9351348d0608fda25088d7130ed
XiBinBin <1290212854@qq.com>
2018-07-27 09:26:04 +0800

    RegisterRequest.cs

---
+++ Assets/Scripts/Request/RegisterRequest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Common;
+
+public class RegisterRequest : BaseRequest
+{
+    private RegisterPanel registerPanel;
+    public override void Awake()
+    {
+        registerPanel = GetComponent<RegisterPanel>();
+        requestCode = RequestCode.User;
+        actionCode = ActionCode.Register;
+        base.Awake();
+    }
+    public void SendRequest(string username, string password)
+    {
+        string data = username + "," + password;
+        base.SendRequest(data);
+    }
+    public override void OnResponse(string data)
+    {
+        ReturnCode returnCode = (ReturnCode)int.Parse(data);
+        registerPanel.OnRegisterResponse(returnCode);
+    }
+}
