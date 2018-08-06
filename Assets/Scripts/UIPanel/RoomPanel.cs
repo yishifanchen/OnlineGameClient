3840717a6090b13ade905b8530021c569bacda11
XiBinBin <1290212854@qq.com>
2018-08-06 18:20:06 +0800

    RoomPanel

---
+++ Assets/Scripts/UIPanel/RoomPanel.cs
@@ -0,0 +1,52 @@
+﻿using Common;
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomPanel : BasePanel
+{
+
+    private void Start()
+    {
+        transform.Find("ImageBG/Buttons/ButtonQuit").GetComponent<Button>().onClick.AddListener(OnCloseBtnClick);
+    }
+    public override void OnEnter()
+    {
+        base.OnEnter();
+        EnterAnim();
+    }
+    public override void OnPause()
+    {
+        base.OnPause();
+        HideAnim();
+    }
+    public override void OnResume()
+    {
+        base.OnResume();
+        EnterAnim();
+    }
+    public override void OnExit()
+    {
+        base.OnExit();
+        HideAnim();
+    }
+    private void OnCloseBtnClick()
+    {
+        uiMng.PopPanel();
+    }
+    private void EnterAnim()
+    {
+        gameObject.SetActive(true);
+        transform.localPosition = new Vector3(-2000, 0, 0);
+        transform.DOLocalMove(Vector3.zero, 0.3f);
+        transform.localScale = Vector3.zero;
+        transform.DOScale(1, 0.3f);
+    }
+    private void HideAnim()
+    {
+        transform.DOScale(0, 0.3f);
+        transform.DOLocalMove(new Vector3(-2000, 0, 0), 0.3f).OnComplete(() => gameObject.SetActive(false));
+    }
+}
